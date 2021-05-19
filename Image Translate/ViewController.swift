56475e2ea0fbe1564f69253dc819edee9223e42d
Maksym Kuba <maximkuba@gmail.com>
2021-05-19 12:19:44 +0300

    Initial Commit

---
+++ Image Translate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Translate
+//
+//  Created by Maxim Kuba on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
